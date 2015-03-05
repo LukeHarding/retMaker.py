75ffeddd7a1810f55aeb0881d239df1a21fc9d26
LukeHarding <harding.luke.aaron@gmail.com>
2015-03-05 13:52:45 +0000

    This is the script

---
+++ retMaker.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import string
+import sys
+
+addr = sys.argv[1]
+
+addr = str(addr)
+
+fb = addr[0:2]
+sb = addr[2:4]
+tb = addr[4:6]
+fob = addr[6:8]
+
+print "\\x" + fob + "\\x" + tb + "\\x" + sb + "\\x" + fb
