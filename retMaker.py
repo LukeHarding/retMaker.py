95db66113e7790cadccd37a328cbf1d663b0a1a0
LukeHarding <Harding.luke.aaron@gmail.com>
2015-03-24 08:29:15 -0400

    Added input checking

--- retMaker.py
+++ retMaker.py
@@ -11,4 +11,7 @@ sb = addr[2:4]
 tb = addr[4:6]
 fob = addr[6:8]
 
-print "\\x" + fob + "\\x" + tb + "\\x" + sb + "\\x" + fb
+if len(addr) == 8:
+    print "\\x" + fob + "\\x" + tb + "\\x" + sb + "\\x" + fb
+else:
+    print "Nope, that was bad input."
